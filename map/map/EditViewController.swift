b8f8ad578ca056d641e7f3995066fa7390b724d6
Tairascii <64748528+Tairascii@users.noreply.github.com>
2021-03-26 23:18:40 +0600

    Add files via upload

---
+++ map/map/EditViewController.swift
@@ -0,0 +1,38 @@
+//
+//  EditViewController.swift
+//  map
+//
+//  Created by Tair Sairanbekov on 24.03.2021.
+//
+
+import UIKit
+import MapKit
+
+protocol EditPinDelegate {
+    func edit(name: String, desc: String, coordinate: CLLocationCoordinate2D)
+}
+
+class EditViewController: UIViewController {
+
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    var myDelegate: EditPinDelegate?
+    var name: String?
+    var desc: String?
+    var coordinate: CLLocationCoordinate2D?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationTextField.text = name!
+        descriptionTextField.text = desc!
+    }
+    
+    @IBAction func saveButton(_ sender: UIBarButtonItem) {
+        myDelegate?.edit(name: locationTextField.text!, desc: descriptionTextField.text!, coordinate: coordinate!)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cancelButton(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
---
+++ map/map/Table.swift
@@ -0,0 +1,43 @@
+//
+//  Table.swift
+//  map
+//
+//  Created by Tair Sairanbekov on 24.03.2021.
+//
+
+import Foundation
+import MapKit
+
+extension MapViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pins.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Location", for: indexPath)
+        cell.textLabel?.text = pins[indexPath.row].name
+        cell.detailTextLabel?.text = pins[indexPath.row].desc
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        myTableView.deselectRow(at: indexPath, animated: true)
+        myTableView.isHidden = true
+        showAnn(index: indexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 75
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let coordinate = getXY(index: indexPath.row)
+            delete(object: pins[indexPath.row])
+            loadPins()
+            myMap.removeAnnotation(getAnn(coordinate: coordinate))
+            myTableView.reloadData()
+        }
+    }
+}
+
